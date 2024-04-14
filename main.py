23d2b8d7c43d897dec767d215fb21160dd90a826
TyDavisson <tdavisson0@gmail.com>
2024-04-14 02:55:13 -0400

    finished project, still want to add comments

--- main.py
+++ main.py
@@ -2,29 +2,42 @@ def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     num_words = get_words(text)
-    num_letters = get_letters(text)
+    letters_dict = get_letters_dict(text)
+    print(f"--- Begin report of {path} ---")
     print(f"{num_words} words")
-    print(num_letters)
-
-def get_words(text):
-    words = text.split()
-    return len(words)
-
-def get_letters(text):
-    letters = {}
-    for c in text:
-        lower = c.lower()
-        if lower in letters:
-            letters[lower] += 1
-        else:
-            letters[lower] = 1
-    return letters
+    print()
+    get_report(letters_dict)
+    print("--- End report ---")
+    
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
-def report(dict):
+def get_words(text):
+    words = text.split()
+    return len(words)
+
+def get_letters_dict(text):
+    letters_dict = {}
+    for c in text:
+        lower = c.lower()
+        if lower in letters_dict:
+            letters_dict[lower] += 1
+        else:
+            letters_dict[lower] = 1
+    return letters_dict
+  
+def sort_on(dict):
+    return dict["num"]
     
-    
+def get_report(dict):
+    report = []
+    for letter in dict:
+        if letter.isalpha():
+            report.append({"letter": letter, "num": dict[letter]})
+    report.sort(reverse=True, key=sort_on)
+    for line in report:
+        print(f"Letter: {line['letter']}, Count: {line["num"]}")
+          
 main()
